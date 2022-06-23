19f2471ce354c7bdbd1f458078a60db3ecbdb6fe
PratyaInrod <pratya.inr@gmail.com>
2022-06-23 22:24:41 +0700

    Commit the Publisher JPA Update

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,13 +1,9 @@
 package guru.springframework.spring5webapp.domain;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+
 
 
 
